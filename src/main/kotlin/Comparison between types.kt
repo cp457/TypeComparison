15ed550baa38029af1009fe350d0ee8bd1d6381c
[Chris] <[christianpriolo96@gmail.com]>
2022-12-20 13:55:38 +0100

    ComparisonTypes

---
+++ src/main/kotlin/Comparison between types.kt
@@ -0,0 +1,12 @@
+class Comparisonbetweentypes {
+}
+
+fun main() {
+
+    val f1: Int = 5
+    val f2: Double = 3.4
+    val f3: Short = 1
+
+    println("the difference is ${f1.equals(f2)}")
+    println("the difference is ${f2.equals(f3)}")
+}
